f11a839fe15c34a54bb591178404a62c8c590e82
Alastair Danley-Cox <AliC@users.noreply.github.com>
2018-11-17 08:26:11 +0000

    Added first test.  Requires SQL LocalDB

---
+++ Persistence.Dapper/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Persistence.Dapper
+{
+    public class Database
+    {
+        protected static string GetConnectionString(string databaseName = null)
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "(LocalDB)\\MSSQLLocalDB";
+            connectionStringBuilder.IntegratedSecurity = true;
+            connectionStringBuilder.ConnectTimeout = 30;
+            connectionStringBuilder.InitialCatalog = databaseName ?? "master";
+
+            connectionStringBuilder.Pooling = false;
+
+            return connectionStringBuilder.ConnectionString;
+        }
+
+        internal static IDbConnection GetConnection()
+        {
+            return new SqlConnection(GetConnectionString("Dapper"));
+        }
+    }
+}
---
+++ Persistence.Dapper/OrderTests.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Persistence.Dapper
+{
+    [TestFixture]
+    public class OrderTests
+    {
+
+        [Test]
+        public void InsertOrder()
+        {
+            IEnumerable<Order> orders;
+
+            using (IDbConnection connection = Database.GetConnection())
+            {
+                var newId = connection.Insert(new Order { ProductId = 1, ContactName = "Alastair" });
+
+                orders = connection.Query<Order>("SELECT * FROM Orders WHERE Id = @Id", new { Id = newId });
+            }
+
+            
+            Assert.That(orders, Is.Not.Null);
+        }
+    }
+}
